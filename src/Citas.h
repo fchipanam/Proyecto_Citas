051dfd0868c01f399e2b87fc8f568fbd367fff13
cristophercauna <ccaunam@unjbg.edu.pe>
2025-12-17 09:21:28 -0500

    Carpeta src

---
+++ src/Citas.h
@@ -0,0 +1,37 @@
+#ifndef CITAS_H
+#define CITAS_H
+#include<iostream>
+#include<string>
+#include<sstream>
+using namespace std;
+
+class Cita{
+    protected:
+        string id;
+        string dniPaciente;
+        string nombrePaciente;
+        int prioridad;
+    public:
+        Cita(string  _id, string dni,string nombre, int pri ){
+            id=_id;
+            dniPaciente=dni;
+            nombrePaciente=nombre;
+            prioridad=pri;
+        }
+        string getDNI(){
+            return dniPaciente;
+        }
+        int getPrioridad(){
+            return prioridad;
+        }
+        string getNombre(){
+            return nombrePaciente;
+        }
+        virtual string obtenerDetalles()const = 0;
+
+        virtual string formatoArchivo() const{
+        return id + "|" + dniPaciente + "|" + nombrePaciente + "|" + to_string(prioridad);
+        }
+        virtual ~Cita(){}
+};
+#endif
