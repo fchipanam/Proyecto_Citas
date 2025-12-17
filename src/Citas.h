4b1142ff55789d2e5f661824ae1c18dcce1e4750
cristophercauna <ccaunam@unjbg.edu.pe>
2025-12-17 09:22:54 -0500

    Se añade la clase hija con sus respectivos atributos y metodos(ya definidos) .

--- src/Citas.h
+++ src/Citas.h
@@ -34,5 +34,29 @@ class Cita{
         }
         virtual ~Cita(){}
 };
+class CitaPresencial : public Cita{
+    private:
+        string especialidad;
+        int numeroConsultorio;
+    public:
+        CitaPresencial(string _id, string dni,string nombre,int prio,string esp,int cons):Cita(_id,dni,nombre,prio){
+            especialidad=esp;
+            numeroConsultorio=cons;
+        }
+        string obtenerDetalles()const override{
+            string descPrioridad;
+            if(prioridad==1){
+                descPrioridad="URGENCIA";
+            }else if(prioridad==2){
+                descPrioridad=="Normal";
+            }else{
+                descPrioridad=="Baja";
+            }
+            return "ID: " + id + " | Paciente: " + nombrePaciente + " (DNI: " + dniPaciente + ")\n" + "   -> Tipo: Presencial | Consultorio: " + to_string(numeroConsultorio) +" | Especialidad: " + especialidad + " | Prioridad: " + descPrioridad + "\n";
+        }
+        string formatoArchivo() const override{
+            return "P|" + Cita::formatoArchivo() + "|" + especialidad + "|" + to_string(numeroConsultorio);
+        }
+};
 
 #endif
